7f2733225f37376386285382899974f180a96bdc
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-05-19 07:50:55 -0700

    ported to video instead of canvas

--- express/express-node/public/camera.js
+++ express/express-node/public/camera.js
@@ -41333,6 +41333,9 @@ async function bindPage() {
 
   try {
     video = await loadVideo();
+    var x = output.captureStream();
+    targetV.srcObject = x;  
+    targetV.play();
   } catch (e) {
     let info = document.getElementById('info');
     info.textContent = 'this browser does not support video capture,' + 'or this device does not have a camera';
